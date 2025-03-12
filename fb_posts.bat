ace5acda82e472710c0a3fa8028c47654ed0e2f1
Amy Carlson <automate.analyze@gmail.com>
2025-03-12 16:34:49 -0500

    corrected bat file

--- fb_posts.bat
+++ fb_posts.bat
@@ -1,2 +1,6 @@
-python "C:\Development\Portfolio\SocialMedia_Automation\fb_posts.py"
+@echo off
+cd /d C:\Development\Portfolio\SocialMedia_Automation
+call sm_auto\Scripts\activate.bat
+sm_auto\Scripts\python.exe fb_posts.py
+deactivate
 pause
